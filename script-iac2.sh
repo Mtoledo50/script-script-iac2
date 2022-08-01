3e2c5d30c8ffbf7408183c37ad86a5f9fd483ad5
Marcos Toledo <104846839+Mtoledo50@users.noreply.github.com>
2022-08-01 12:08:44 -0300

    Update script-iac2.sh
    Primeira versao

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,2 +1,18 @@
-#!/bin/bash
-ap
+#!/bin/bas
+echo "fazendo atualização do servidor.."
+apt-get update
+apt-get upgrade -y
+echo "baixando e copiando os arquivos da aplicação.."
+apt-get install apache2 -y
+apt-get install unzip -y
+apt-get install wget -y
+echo "fazendo o download do main.zip do git do denilson"
+cd /tmp
+wget https//github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cd -r * /var/www/html/
+
+‐------------
+
+Chmod +X script-iac2.sh
